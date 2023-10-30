b907592b2bd6a6000e70eea3b39c0050fb76a92f
LOUISENZR <louisanaza@gmail.com>
2023-10-30 09:48:31 +0700

    Second commit

--- ArrayNilai16.java
+++ ArrayNilai16.java
@@ -5,13 +5,19 @@ public class ArrayNilai16 {
     Scanner sc = new Scanner(System.in);
     int[] nilaiAkhir = new int[10];
 
-    for (int i = 0; i < 10; i++){
-        System.out.println("Masukan nilai akhir ke-"+i+" : ");
+        for (int i = 0; i < nilaiAkhir.length; i++){
+        System.out.print("Masukan nilai akhir ke-"+i+" : ");
         nilaiAkhir[i] = sc.nextInt();
-    }
-    for (int i= 0; i < 10; i++){
-        System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
-    }
-    }
+        }
+        for (int i= 0; i < nilaiAkhir.length; i++){
+        if(nilaiAkhir[i] > 70){
+            System.out.println("Mahasiswa ke-"+i+" lulus! ");
+        }else 
+            System.out.println("Mahasiswa ke-"+i+" tidak lulus! ");
+
+        }
+        sc.close();
     
-}
+        }
+    }
+
