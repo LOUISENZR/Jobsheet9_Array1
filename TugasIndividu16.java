9aa871e4fdaaed931175e4d3f20a441a4c0a939b
LOUISENZR <louisanaza@gmail.com>
2023-10-30 12:03:19 +0700

    Third commit

--- TugasIndividu16.java
+++ TugasIndividu16.java
@@ -1,4 +1,44 @@
 import java.util.Scanner;
+
 public class TugasIndividu16 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int banyElm = 0;
+        double rata2 = 0;
+        int nilTerendah = 0;
+        int nilTertinggi = 0;
+        int total = 0;
+
+        System.out.println("Masukkan Banyak Elemen :");
+        banyElm = sc.nextInt();
+        int[] nilElm = new int[banyElm];
+
+        for (int i = 0; i < nilElm.length; i++) {
+            System.out.print("Masukkan Nilai Elemen ke-" + (i + 1) + " : ");
+            nilElm[i] = sc.nextInt();
+
+            if (i == 0) {
+                // Inisialisasi nilai tertinggi dan terendah dengan nilai pertama
+                nilTertinggi = nilElm[i];
+                nilTerendah = nilElm[i];
+            }
+
+            if (nilElm[i] > nilTertinggi) {
+                nilTertinggi = nilElm[i];
+            }
+            if (nilElm[i] < nilTerendah) {
+                nilTerendah = nilElm[i];
+            }
+
+            total += nilElm[i];
+        }
+
+        rata2 = (double) total / banyElm;
+
+        System.out.println("Nilai tertinggi: " + nilTertinggi);
+        System.out.println("Nilai terendah: " + nilTerendah);
+        System.out.println("Rata-rata nilai: " + rata2);
+
+        sc.close();
+    }
 }
